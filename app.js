7137510feb26f1633a385cd6266059bd79bdf6db
Berdan Deniz Bolat <berdandenizbolat@gmail.com>
2021-12-23 15:51:52 +0300

    change port declaration

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'))
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.use(express.static(__dirname));
-const port = process.env.port || 3000
-app.listen(port);
+app.listen(process.env.port || 3000);
 
-console.log('Server is running at port ' + port);
+console.log('Server is running");
